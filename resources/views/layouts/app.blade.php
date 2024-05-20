d60989f0bb6fccc7d04c0b7685081f69f6b97f1a
root <donthimas54@gmail.com>
2024-05-21 00:10:47 +0200

    adding files

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @livewireStyles
+</head>
+<body>
+    <a href="/admin/dashboard">click to dashboard</a>
+
+    <h1>Hei im Laravel</h1>
+
+    {{-- {{ $slot }} --}}
+
+
+
+    @livewireScripts
+</body>
+</html>
